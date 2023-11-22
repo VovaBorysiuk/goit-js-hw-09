0a7dbc103800eaa824b1ff5256e8fae78035a74c
Vova <v.borysiuk91@gmail.com>
2023-11-22 18:38:10 +0200

    firts task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,43 @@
-console.log("777")
+const refs = {
+  btnStart: document.querySelector('button[data-start]'),
+  btnStop: document.querySelector('button[data-stop]'),
+};
+
+let intervalID;
+//=============================================================
+
+refs.btnStart.style.fontSize = '20px';
+refs.btnStop.style.fontSize = '20px';
+refs.btnStart.style.padding = '10px';
+refs.btnStop.style.padding = '10px';
+
+//=============================================================
+refs.btnStop.disabled = true;
+
+refs.btnStart.addEventListener('click', startChangeColor);
+refs.btnStop.addEventListener('click', stopChangeColor);
+
+//===============================================================
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+//==============================================================
+function startChangeColor() {
+    intervalID = setInterval(changeBodyColor, 1000);
+    refs.btnStart.disabled = true;
+    refs.btnStop.disabled = false;
+    
+}
+//=============================================================
+function stopChangeColor() {
+    clearTimeout(intervalID)
+    refs.btnStart.disabled = false;
+    refs.btnStop.disabled = true;
+  
+}
+//=============================================================
+function changeBodyColor() {
+    document.body.style.backgroundColor = getRandomHexColor()
+}
