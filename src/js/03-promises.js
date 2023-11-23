9442af12898a6f30a950022fb1dedf47d951d245
Vova <v.borysiuk91@gmail.com>
2023-11-23 22:41:08 +0200

    rewrote third task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,48 +1,37 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-const refs = {
-  delayEl: document.querySelector('[name="delay"]'),
-  stepEl: document.querySelector('[name="step"]'),
-  amountEl: document.querySelector('[name="amount"]'),
-  submitBtnEl: document.querySelector('button'),
-};
 
-refs.submitBtnEl.addEventListener('click', onCreatePromiseBtn);
-
-//================================================================
-function createPromise(position, delay, step) {
+function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
     const shouldResolve = Math.random() > 0.3;
-    const timeDelay = delay + (position - 1) * step;
-
     setTimeout(() => {
       if (shouldResolve) {
-        resolve(`✅ Fulfilled promise ${position} in ${timeDelay} ms`);
+        resolve({ position, delay });
       } else {
-        reject(`❌ Rejected promise ${position} in ${timeDelay} ms`);
+        reject({ position, delay });
       }
-    }, timeDelay);
+    }, delay);
   });
 }
 
-//================================================================
-function onCreatePromiseBtn(event) {
-  event.preventDefault();
+const promiseForm = document.querySelector('.form')
 
-  const delay = Number(refs.delayEl.value);
-  const step = Number(refs.stepEl.value);
-  const amount = Number(refs.amountEl.value);
+promiseForm.addEventListener('submit', async e => {
+  e.preventDefault();
+
+  const delay = +e.target.elements.delay.value;
+  const step = +e.target.elements.step.value;
+  const amount = +e.target.elements.amount.value;
+
 
   for (let i = 1; i <= amount; i++) {
-    createPromise(i, delay, step)
-      .then(message => {
-        Notify.success(message);
-        console.log(message);
-      })
-      .catch(error => {
-        Notify.failure(error);
-        console.log(error);
-      });
+    const currentDelay = delay + (i - 1) * step;
+    try {
+      await createPromise(i, currentDelay);
+      Notify.success(`✅ Fulfilled promise ${i} in ${currentDelay}ms`);
+    } catch (error) {
+      Notify.failure(`❌ Rejected promise ${i} in ${currentDelay}ms`);
+    }
   }
-}
 
+});
